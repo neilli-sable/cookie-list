a5039d9acaaa6d6a8aa9871a604db97dfb6983bc
Seiya Sakaguchi <sakaguchi@inas-net.co.jp>
2016-09-13 11:59:14 +0900

    fix event add

--- cookie-list.js
+++ cookie-list.js
@@ -1,46 +1,16 @@
 var cookieList = (function() {
   var _list = [];
   var _key = 'favorite';
-  var _libName = 'cookie-list';
 
   var init = function() {
     _list = [];
-    load(function() {
-      var list = document.getElementById(_libName).getElementsByTagName('li');
-      [].forEach.call(list, function(item) {
-        var id = item.getElementsByClassName(_libName + '-id')[0].textContent;
-        var button = item.getElementsByClassName(_libName + '-button')[0];
-        if (cookieList.list().indexOf(id) > -1) {
-          button.style.color = '#ffff00';
-        }
-      });
-      addEvent();
-    });
+    load();
   };
 
-  var addEvent = function() {
-    var buttons = document.getElementsByClassName(_libName + '-button');
-    [].forEach.call(buttons, function(aButton) {
-      aButton.addEventListener('click', function() {
-        // list control
-        var star = cookieList.toggle(this.parentNode.getElementsByClassName(_libName + '-id')[0].textContent);
-        cookieList.save();
-
-        // icon change
-        if (star) {
-          this.style.color = '#ffff00';
-        } else {
-          this.style.color = '#000000';
-        }
-      });
-    });
-  };
-
-  var load = function(callback) {
+  var load = function() {
     if (cookieControl.toHash().favorite) {
       _list = cookieControl.toHash().favorite.split(',');
     }
-    callback();
   };
 
   var toggle = function(data) {
